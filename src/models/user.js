1c282857bf79b2d76b26209a965ccfac50c27998
preethiviy <preethivirajyambem@gmail.com>
2025-01-24 22:12:57 +0530

    user schema methods, validation, password encryption

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema(
     {
@@ -17,10 +20,20 @@ const userSchema = new mongoose.Schema(
             required: true,
             unique: true,
             trim: true,
+            validate(value) {
+                if (!validator.isEmail(value)) {
+                    throw new Error("Invalid email address: " + value);
+                }
+            },
         },
         password: {
             type: String,
             required: true,
+            validate(value) {
+                if (!validator.isStrongPassword(value)) {
+                    throw new Error("Enter a Strong Password: " + value);
+                }
+            },
         },
         age: {
             type: Number,
@@ -43,6 +56,11 @@ const userSchema = new mongoose.Schema(
         photoUrl: {
             type: String,
             default: "https://geographyandyou.com/images/user-profile.png",
+            validate(value) {
+                if (!validator.isURL(value)) {
+                    throw new Error("Invalid Photo URL: " + value);
+                }
+            },
         },
         about: {
             type: String,
@@ -56,6 +74,28 @@ const userSchema = new mongoose.Schema(
         timestamps: true,
     }
 );
+
+userSchema.methods.getJWT = async function () {
+    const user = this;
+
+    const token = await jwt.sign({ _id: user._id }, "DEV@Tinder$790", {
+        expiresIn: "7d",
+    });
+
+    return token;
+};
+
+userSchema.methods.validatePassword = async function (passwordInputByUser) {
+    const user = this;
+    const passwordHash = user.password;
+
+    const isPasswordValid = await bcrypt.compare(
+        passwordInputByUser,
+        passwordHash
+    );
+
+    return isPasswordValid;
+};
 
 const userModel = mongoose.model("user", userSchema);
 
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,26 +2,33 @@ const express = require("express");
 const authRouter = express.Router();
 
 const User = require("../models/user");
+const { validateSignUpData } = require("../utils/validation");
+const bcrypt = require("bcrypt");
 
 authRouter.post("/signup", async (req, res) => {
     try {
         // Validation of data
-        // todo
+        validateSignUpData(req);
 
         const { firstName, lastName, emailId, password } = req.body;
 
         // Encrypt the password
-        // todo
+        const passwordHash = await bcrypt.hash(password, 10);
 
         //   Creating a new instance of the User model
         const user = new User({
             firstName,
             lastName,
             emailId,
-            password,
+            password: passwordHash,
         });
 
         const savedUser = await user.save();
+        const token = await savedUser.getJWT();
+
+        res.cookie("token", token, {
+            expires: new Date(Date.now() + 8 * 3600000),
+        });
 
         res.json({ message: "User Added successfully!", data: savedUser });
     } catch (err) {
